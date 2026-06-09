53fd948ff72f12579ab63102ad043a9d78880833
bakhtiyarbhai03-cmyk <bakhtiyarbhai03@gmail.com>
2026-06-09 22:13:34 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'height-meter-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached file if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
